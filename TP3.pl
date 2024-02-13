00bc2295ed6f0df75abd67fb1d1ef0cacbe03c39
Mathieu Aubin <maubin@enssat.fr>
2024-02-13 11:33:37 +0100

    End pb1

--- TP3.pl
+++ TP3.pl
@@ -1,26 +1,34 @@
 /* ----- Problème 1 ----- */
 
-/* Prédicats */
+/* Prédicats */ 
+/* Enlève X de la liste L pour obtenir L1 */
 enlever(X, [X|Q], Q).
 enlever(X, [Y|Q], [Y|Q1]):- enlever(X, Q, Q1).
 
-permut([], [].).
-permut (L, [X|L2]):- enlever(X, L, L1), permut(L1, L2).
+/* Permutation de la liste L */
+permut([], []).
+permut(L, [X|L2]):- enlever(X, L, L1), permut(L1, L2).
 
-voisin(X, Y, [X, Y|Q]).
-voisin(X, Y, [T|Q]):- voisin(X, Y, Q).
-voisin(X, Y, L):- voisin(Y, X, L).
+/* Prédicats de test */
+griffe(chat).
+griffe(lion).
 
-entre(X, Y, Z, L):- voisin(X, Z, L), voisin(Z, Y, L)
+/* Règles */
+aCote(X,Y,[X,Y|Q]).
+aCote(X,Y,[Y,X|Q]).
+aCote(X,Y,[T|Q]):-aCote(X,Y,Q).
 
-/* Données */
-voisin(canard, chat, L);
-entre(serpent, pingouin, ver, L).
-entre(ver, lion, pinguoin).
+entre(X,Y,Z,[X,Y,Z|Q]).
+entre(X,Y,Z,[Z,Y,X|Q]).
+entre(X,Y,Z,[T|Q]):-entre(X,Y,Z,Q).
 
-boite([X1, X2, X3, X4, X5, X6]):- L = [ver, serpent, lion, canard, chat, pingouin],
-                                  permut(L, [X1, X2, X3, X4, X5, X6]),
-                                  permut([lion, chat], [X2, X3]),
-                                  voisin(canard, chat, L),
-                                  entre(serpent, pingouin, ver, L),
-                                  entre(ver, lion, pinguoin).
+boite([X1, X2, X3, X4, X5, X6]):- % Génération de la liste des animeaux
+                                  permut([chat, lion, serpent, canard, ver, pingouin], [X1, X2, X3, X4, X5, X6]),
+                                  % Animal à griffes à la 2ème ou 3ème position
+                                  griffe(X2),griffe(X3), 
+                                  % Le serpent est à côté du canard
+                                  aCote(canard,chat,[X1, X2, X3, X4, X5, X6]),
+                                  % Le ver est entre le serpent et le pinguoin
+                                  entre(serpent,ver,pingouin,[X1, X2, X3, X4, X5, X6]),
+                                  % Le pinguoin est entre le ver et le lion
+                                  entre(ver,pingouin,lion,[X1, X2, X3, X4, X5, X6]).
